804421e57dca14a39ea09dfa4efdac826a74c99d
iribeirodev <ribeirojr.itamar@gmail.com>
2020-06-16 19:02:53 -0300

    saved

--- numberguess.go
+++ numberguess.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	var guess int
+	var guess, generated int
 	var limi, limf int = 1, 9999
+	var lives = 3
 
-	fmt.Println("Adivinhar Numero")
+	fmt.Printf("Adivinhar Numero - Vidas: %d\n", lives)
+
+	rand.Seed(time.Now().UnixNano())
 	limf = rand.Intn(limf-limi) + limi
+
+	generated = rand.Intn(limf-limi) + limi
 
 	fmt.Printf("Adivinhe um numero entre %d e %d: ", limi, limf)
 	fmt.Scanf("%d", &guess)
-
+	fmt.Printf("Gerado: %d\n", generated)
 }
