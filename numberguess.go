9e9b4ea7f20fdf0625173548501c93a8a0c8bfe4
iribeirodev <ribeirojr.itamar@gmail.com>
2020-06-17 19:16:23 -0300

    Working on game logic

--- numberguess.go
+++ numberguess.go
@@ -4,21 +4,83 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+  "strings"
 )
 
-func main() {
-	var guess, generated int
-	var limi, limf int = 1, 9999
-	var lives = 3
+var limi, limf int = 1, 9999
+var lives int = 5
+var guess, generated int
 
-	fmt.Printf("Adivinhar Numero - Vidas: %d\n", lives)
-
-	rand.Seed(time.Now().UnixNano())
-	limf = rand.Intn(limf-limi) + limi
-
-	generated = rand.Intn(limf-limi) + limi
-
+func obterPalpite() {
 	fmt.Printf("Adivinhe um numero entre %d e %d: ", limi, limf)
 	fmt.Scanf("%d", &guess)
-	fmt.Printf("Gerado: %d\n", generated)
+}
+
+func gerarResposta() {
+	generated = rand.Intn(limf-limi) + limi
+  for generated % 10 != 0 {
+    fmt.Printf("gerando numero: %d\n", generated)    
+    generated = rand.Intn(limf-limi) + limi
+  }
+
+  fmt.Printf("O GERADO FOI %d\n", generated)
+}
+
+func controlarVidas() {
+	if guess != generated {
+		configurarLimite()
+		lives--
+	} else {
+    fmt.Printf("PARABÉNS, VOCÊ ACERTOU !!!!  RESPOSTA CORRETA: %d.\n", generated)
+    fmt.Println("VOCÊ É O REI DOS CHUTES. GOSTARIA DE TENTAR MAIS UMA RODADA?")
+  }
+}
+
+func configurarLimite() {
+  if guess > generated {
+    limf = guess
+  } else if guess < generated {
+    limi = guess
+  }
+}
+
+func exibirRelatorio() {
+  delimi := strings.Repeat("~", 80)
+  clearScreen()
+
+  fmt.Println(delimi)
+
+  fmt.Println("Acabaram-se as vidas, jogo terminado.")
+  fmt.Printf("O número correto era %d\n", generated)
+  fmt.Println("Melhor sorte da próxima vez e Muito Obrigado por jogar conosco.")
+
+  fmt.Println(delimi)
+}
+
+func clearScreen() {
+	print("\033[H\033[2J")
+}
+
+func configurarApp() {
+  // Randomizacao
+  rand.Seed(time.Now().UnixNano())
+  // Limpando a tela
+  clearScreen()
+}
+
+func exibirScore() {
+  fmt.Printf("Adivinhar Numero - Vidas: %d\n", lives)
+}
+
+func main() {
+  configurarApp()
+	gerarResposta()
+
+	for lives > 0 {
+    exibirScore()
+		obterPalpite()
+		controlarVidas()
+	}
+
+  exibirRelatorio()
 }
