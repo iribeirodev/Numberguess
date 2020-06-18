1049aa50b93234bd602f0f1097b9a2e2adb13680
iribeirodev <ribeirojr.itamar@gmail.com>
2020-06-18 18:02:36 -0300

    Committing on master

--- numberguess.go
+++ numberguess.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"C"
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
-  "strings"
 )
 
 var limi, limf int = 1, 9999
 var lives int = 5
 var guess, generated int
+var acertou bool = false
 
 func obterPalpite() {
 	fmt.Printf("Adivinhe um numero entre %d e %d: ", limi, limf)
@@ -18,12 +20,11 @@ func obterPalpite() {
 
 func gerarResposta() {
 	generated = rand.Intn(limf-limi) + limi
-  for generated % 10 != 0 {
-    fmt.Printf("gerando numero: %d\n", generated)    
-    generated = rand.Intn(limf-limi) + limi
-  }
+	for generated%10 != 0 {
+		generated = rand.Intn(limf-limi) + limi
+	}
 
-  fmt.Printf("O GERADO FOI %d\n", generated)
+	//fmt.Printf("O GERADO FOI %d\n", generated)
 }
 
 func controlarVidas() {
@@ -31,30 +32,34 @@ func controlarVidas() {
 		configurarLimite()
 		lives--
 	} else {
-    fmt.Printf("PARABÉNS, VOCÊ ACERTOU !!!!  RESPOSTA CORRETA: %d.\n", generated)
-    fmt.Println("VOCÊ É O REI DOS CHUTES. GOSTARIA DE TENTAR MAIS UMA RODADA?")
-  }
+		acertou = true
+	}
 }
 
 func configurarLimite() {
-  if guess > generated {
-    limf = guess
-  } else if guess < generated {
-    limi = guess
-  }
+	if guess > generated {
+		limf = guess
+	} else if guess < generated {
+		limi = guess
+	}
+}
+
+func delimitar() {
+	var delimi = strings.Repeat("~", 80)
+	fmt.Println(delimi)
 }
 
 func exibirRelatorio() {
-  delimi := strings.Repeat("~", 80)
-  clearScreen()
-
-  fmt.Println(delimi)
-
-  fmt.Println("Acabaram-se as vidas, jogo terminado.")
-  fmt.Printf("O número correto era %d\n", generated)
-  fmt.Println("Melhor sorte da próxima vez e Muito Obrigado por jogar conosco.")
-
-  fmt.Println(delimi)
+	clearScreen()
+	delimitar()
+	if !acertou {
+		fmt.Println("Acabaram-se as vidas, jogo terminado.")
+		fmt.Printf("O número correto era %d\n", generated)
+		fmt.Println("Melhor sorte da próxima vez e Muito Obrigado por jogar conosco.")
+	} else {
+		fmt.Printf("PARABÉNS, VOCÊ ACERTOU !!!!  RESPOSTA CORRETA: %d.\n", generated)
+	}
+	delimitar()
 }
 
 func clearScreen() {
@@ -62,25 +67,28 @@ func clearScreen() {
 }
 
 func configurarApp() {
-  // Randomizacao
-  rand.Seed(time.Now().UnixNano())
-  // Limpando a tela
-  clearScreen()
+	// Randomizacao
+	rand.Seed(time.Now().UnixNano())
+	// Limpando a tela
+	clearScreen()
 }
 
 func exibirScore() {
-  fmt.Printf("Adivinhar Numero - Vidas: %d\n", lives)
+	fmt.Printf("Adivinhar Numero - Você ainda tem %d Vida(s).\n", lives)
 }
 
 func main() {
-  configurarApp()
+	configurarApp()
 	gerarResposta()
 
 	for lives > 0 {
-    exibirScore()
+		exibirScore()
 		obterPalpite()
 		controlarVidas()
+		if acertou {
+			break
+		}
 	}
 
-  exibirRelatorio()
+	exibirRelatorio()
 }
